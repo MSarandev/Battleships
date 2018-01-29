7f9b5181e74184099a6ba806e43c73520de0b24c
MSarandev <maxsarandev@gmail.com>
2018-01-29 16:16:26 +0000

    Initial Ship write-up

--- SarandevBattleships/Program.cs
+++ SarandevBattleships/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace SarandevBattleships
+﻿// Created by Maxim Sarandev
+// Jan 2018
+// Custom Software for BEED Gaming. All rights reserved
+
+using System;
+
+namespace SarandevBattleships
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            // Main application method
             
+            var ship1 = new Ship("alpha",4,90,0,0);
+            
+            ship1.print_me();
+            
+            ship1.Deconstruct(ship1);
         }
     }
 }
--- SarandevBattleships/Ship.cs
+++ SarandevBattleships/Ship.cs
@@ -1,7 +1,62 @@
-﻿namespace SarandevBattleships
+﻿// Created by Maxim Sarandev
+// Jan 2018
+// Custom Software for BEED Gaming. All rights reserved
+
+using System;
+
+namespace SarandevBattleships
 {
     public class Ship
     {
+        // Variables Declaration
+        public string id; // unique ship id
+        public string type; // defines the type of ship
+        public int size; // ship size
+        public int rotation; // ship rotation
+        public int pos_x; // position on grid (X coordinate)
+        public int pos_y; // position on grid (Y coordinate)
         
+        // Constructor definition
+        public Ship(string id, int size, int rotation, int posX, int posY)
+        {
+            this.id = id;
+            this.size = size;
+            // Set the type of ship based on the size
+            switch (size)
+            {
+                case 4:
+                    this.type = "Destroyer";
+                    break;
+                case 5:
+                    this.type = "Battleship";
+                    break;
+                default: this.type = "Undefined";
+                    break;
+            }
+
+            this.rotation = rotation;
+            pos_x = posX;
+            pos_y = posY;
+        }
+        
+        
+        // De-constructor definition
+        public void Deconstruct(Ship shp1)
+        {
+            // Deconstruct the object based on the ship parsed
+            id = shp1.id;
+            type = shp1.type;
+            size = shp1.size;
+            rotation = shp1.rotation;
+            pos_x = shp1.pos_x;
+            pos_y = shp1.pos_y;
+        }
+        
+        // Custom methods
+        public void print_me()
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(type);
+        }
     }
 }
